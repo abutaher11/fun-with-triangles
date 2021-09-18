e9dbca1c9c534194627b572255413687942d434a
abu.taher11 <taher2world@gmail.com>
2021-09-18 19:37:41 +0530

    added area of triangle app

---
+++ area.js
@@ -0,0 +1,33 @@
+const sides = document.querySelectorAll(".side-input");
+const calBtn = document.querySelector("#calculate-btn");
+const output = document.querySelector("#output-box");
+
+function calculateArea() {
+    hideMessage();
+    const baseHeight = calBaseIntoHeight(Number(sides[0].value),Number(sides[1].value));
+    const area =  Number(1/2 * baseHeight);
+    if (sides[0].value && sides[1].value && Number(sides[0].value>0) && Number(sides[1].value>0)) {
+        showMessage("The area of the triangle is " + area + " units.");
+    }
+    else {
+        showMessage("Please enter the correct values. Lenght values cannot be negative or empty.");
+    };
+    
+}
+
+function calBaseIntoHeight(a,b) {
+    const baseIntoHeight = a*b;
+    return baseIntoHeight;
+}
+
+function hideMessage () {
+    output.style.display = "none";
+}
+
+function showMessage(msg) {
+    output.style.display = "block";
+    output.innerText = msg;
+}
+
+
+calBtn.addEventListener("click", calculateArea);
